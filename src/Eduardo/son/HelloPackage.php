c4de7b87df503c0c7f25f33e1c639a5f888b7309
Eduardo Brites <egbrites@MacBook-Pro-de-Eduardo.local>
2016-10-05 03:31:49 -0300

    first commit

---
+++ src/Eduardo/son/HelloPackage.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egbrites
+ * Date: 05/10/16
+ * Time: 03:12
+ */
+
+namespace Eduardo\son;
+
+
+class HelloPackage
+{
+	public function getHello(){
+		return "Hello Package!";
+	}
+}
